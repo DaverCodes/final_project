910061f2abf98fcb9d9417b0cb4759b1a7e38549
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-05-30 18:28:57 -0700

    updated starter code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,14 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_MATCHUPS } from '../utils/queries';
+import React from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_MATCHUPS, {
-    fetchPolicy: "no-cache"
-  });
-
-  const matchupList = data?.matchups || [];
-
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome!</h1>
-      </div>
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+      <Cart />
     </div>
   );
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,60 @@
 const { gql } = require('apollo-server-express');
 
-// const typeDefs = gql`
-//   type A {
-//     _id: ID!
-//     name: String!
-//   }
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
 
-//   type Matchup {
-//     _id: ID!
-//     A1: String!
-//     A2: String!
-//     A1_votes: Int
-//     A2_votes: Int
-//   }
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+  }
 
-//   type Query {
-//     A: [A]
-//     matchups(_id: String): [Matchup]
-//   }
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
 
-//   type Mutation {
-//     createMatchup(A1: String!, A2: String!): Matchup
-//     createVote(_id: String!, ANum: Int!): Matchup
-//   }
-// `;
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
+  }
+`;
 
 module.exports = typeDefs;
