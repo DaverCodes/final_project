4f6cddf71b64dbe9cd59f40082c3f203f48b7368
daveS <davesimpcontrol@gmail.com>
2023-06-01 19:11:14 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/minimarket');
+
+module.exports = mongoose.connection;
