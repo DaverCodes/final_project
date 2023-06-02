9085b0265bf8d2f5fef5743a3e6ff7ee79ae3812
daveS <davesimpcontrol@gmail.com>
2023-06-01 21:04:24 -0600

    changed connection.js to not use local host

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/yste');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/yste');
 
 module.exports = mongoose.connection;
