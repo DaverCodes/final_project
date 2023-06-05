62c68606dd870a40c46482e3b8771293ec687a3b
Kyle Shakely <kyleshakely@yahoo.com>
2023-06-05 16:42:19 -0600

    capturing url for listing display, added modal for okay status when image uploads. need to add form

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,17 +1,57 @@
 import React, { useState } from "react";
 import Axios from "axios";
-import { Image } from "cloudinary-react"
+import { Image } from "cloudinary-react";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+const customModalStyles = {
+  content: {
+    width: "300px",
+    height: "200px",
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
 
 function Upload() {
-const [imageSelected, setImageSelected] = useState("")
+  const [imageSelected, setImageSelected] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
 
-  const uploadImage = (files) => {
-    console.log(files[0]);
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const uploadImage = () => {
     const formData = new FormData();
     formData.append("file", imageSelected);
     formData.append("upload_preset", "wayfarer");
-    Axios.post("https://api.cloudinary.com/v1_1/di6pdrfqg/image/upload", formData).then((response)=> console.log(response));
+    setUploading(true);
+
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/di6pdrfqg/image/upload",
+      formData
+    )
+      .then((response) => {
+        console.log(response);
+        const url = response.data.url;
+        setUploading(false);
+        setImageUrl(url);
+        openModal();
+      })
+      .catch((error) => {
+        console.log(error);
+        setUploading(false);
+      });
   };
 
   return (
@@ -22,8 +62,25 @@ const [imageSelected, setImageSelected] = useState("")
           setImageSelected(event.target.files[0]);
         }}
       />
-      <button onClick={uploadImage}>upload image</button>
+      <button onClick={uploadImage}>Upload Image</button>
 
+      {uploading && <p>Please wait for image upload...</p>}
+
+      {imageUrl && (
+        <div>
+          <h3>Listing Photo:</h3>
+          <Image cloudName="di6pdrfqg" publicId={imageUrl} width="300" />
+        </div>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customModalStyles}
+      >
+        <h2>Upload Success!</h2>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
     </div>
   );
 }
