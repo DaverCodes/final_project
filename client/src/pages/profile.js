4f2be1f98da7c5633a795b9a219f0ed8c2da73db
Kyle Shakely <kyleshakely@yahoo.com>
2023-06-05 14:59:56 -0600

    stashing to pull from main

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Image, Transformation, CloudinaryContext } from "cloudinary-react";
+import "./Profile.css";
 
 function Profile() {
-  return (
-    <div>
-      "hello"
-    </div>
-  );
+  render() {
+    return (
+        <div className
+    )
+  }
 }
-
-export default Profile;
