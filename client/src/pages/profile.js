8a3a2b97649926cc3ed14e8d8e03a4675c8c27f0
Kyle Shakely <kyleshakely@yahoo.com>
2023-06-05 19:25:53 -0600

    added form with some basic styling

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Axios from "axios";
 import { Image } from "cloudinary-react";
 import Modal from "react-modal";
+import "./Profile.css"
 
 Modal.setAppElement("#root");
 
@@ -17,7 +18,25 @@ const customModalStyles = {
   },
 };
 
+const categories = [
+  "food",
+  "household supplies",
+  "electronics",
+  "books",
+  "toys",
+  "pharmacy",
+  "pet supplies",
+  "clothing & accessories",
+  "furniture",
+  "sports & outdoors",
+];
+
 function Upload() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
   const [imageSelected, setImageSelected] = useState("");
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [uploading, setUploading] = useState(false);
@@ -30,6 +49,20 @@ function Upload() {
 
   const closeModal = () => {
     setModalIsOpen(false);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form submission logic or API call here
+
+    setName("");
+    setDescription("");
+    setCategory("");
+    setPrice("");
+    setQuantity("");
+    setImageSelected("");
+    setImageUrl("");
   };
 
   const uploadImage = () => {
@@ -47,7 +80,6 @@ function Upload() {
         const url = response.data.url;
         setUploading(false);
         setImageUrl(url);
-        openModal();
       })
       .catch((error) => {
         console.log(error);
@@ -57,16 +89,76 @@ function Upload() {
   };
 
   return (
-    <div>
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageSelected(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImage}>Upload Image</button>
+    <div className="container">
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          >
+            <option value="">Select a category</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            id="price"
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="quantity">Quantity:</label>
+          <input
+            type="number"
+            id="quantity"
+            value={quantity}
+            onChange={(event) => setQuantity(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="image">Image:</label>
+          <input
+            type="file"
+            id="image"
+            onChange={(event) => {
+              setImageSelected(event.target.files[0]);
+            }}
+          />
+          <button type="button" onClick={uploadImage}>
+            Upload Image
+          </button>
+        </div>
+        <p>Once you have finished uploading images, please submit</p>
+        <button type="submit">Submit</button>
+      </form>
 
-      {uploading && <p>Please Wait...</p>}
+      {uploading && <p>Please Wait For Image upload...</p>}
       {error && <p>{error}</p>}
 
       {imageUrl && (
@@ -89,3 +181,4 @@ function Upload() {
 }
 
 export default Upload;
+
